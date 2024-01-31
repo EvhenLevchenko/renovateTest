e41b6ba52ce8070811869b80968b0a19847afa7a
test123 <levjohn1999@gmail.com>
2024-01-31 11:39:07 +0200

    update renovate

--- src/main/java/com/example/renovatetest/RenovateTestApplication.java
+++ src/main/java/com/example/renovatetest/RenovateTestApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RenovateTestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RenovateTestApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RenovateTestApplication.class, args);
+    }
 
 }
